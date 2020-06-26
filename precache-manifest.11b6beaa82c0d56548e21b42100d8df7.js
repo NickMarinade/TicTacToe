84576fe967ffae77ef0efd47af5fdcd4195fb9d7
Mykola Marenich <marenich.mykola@gmail.com>
2020-06-27 00:41:25 +0200

    Updates

---
+++ precache-manifest.11b6beaa82c0d56548e21b42100d8df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d77784d3d528e2296aa806717582f3",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "3a257439b4103c53dcff",
+    "url": "/TicTacToe/static/css/main.417c9cc0.chunk.css"
+  },
+  {
+    "revision": "f707bb854621544093bd",
+    "url": "/TicTacToe/static/js/2.023a328f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.023a328f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a257439b4103c53dcff",
+    "url": "/TicTacToe/static/js/main.eb759d07.chunk.js"
+  },
+  {
+    "revision": "863b3b306981242af621",
+    "url": "/TicTacToe/static/js/runtime-main.727f88c9.js"
+  }
+]);
